4e94ce5eca0c66bb49f1c4d44102e32f8a13ebe3
TexWalker <jimmyewalker@gmail.com>
2018-01-13 11:55:30 +1100

    ex5 complete

---
+++ ex5.rb
@@ -0,0 +1,17 @@
+my_name = 'zed A shaw'
+my_age = 35 #not a lie in 2009
+my_height = 74 #inches
+my_weight = 180 #pounds
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+puts "lets talk about #{my_name}"
+puts "He's #{my_height} inches tall."
+puts "he's #{my_weight} pounds heavy."
+puts "actually thats not too heavy."
+puts "He's got #{my_eyes} eyes and #{my_hair}."
+puts "his teeth are usually #{my_teeth} depending on the coffee."
+
+# This line is tricky, try to get it exactly right
+puts "If i add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}." 
