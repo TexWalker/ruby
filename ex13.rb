fd57524aca3730a8a9edeeea886dc11de9ba9b85
TexWalker <jimmyewalker@gmail.com>
2018-01-23 13:28:05 +1100

    added ex 13

---
+++ ex13.rb
@@ -0,0 +1,8 @@
+first, second, third = ARGV
+
+puts "Your first variable is: #{[first]}"
+puts "Your second variable is: #{[second]}"
+puts "Your third variable is: #{[third]}"
+
+value1 = $stdin.gets.chomp.to_i
+puts "value1"
